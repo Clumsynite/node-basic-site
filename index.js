f0671a18d4b274d1e62e751a634136253d6d13b3
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-24 13:34:33 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+
+http.createServer((req, res) => {
+  const q = url.parse(req.url, true)
+  const filename = q.pathname !== '/' ? `${q.pathname}` : `index`
+  fs.readFile(`.${filename}.html`, (err, data) => {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'})
+      return res.end('<h1>404 not found</h1>')
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    return res.end()
+  })
+}).listen(8080)
